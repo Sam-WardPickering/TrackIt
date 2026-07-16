8b564cd4cc81fd9f941ea7ce0700d4decc5356cb
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-07-16 17:09:39 +1200

    Set up initial unit test framework for permissions

---
+++ tests/unit/logic/permissions.test.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+import { Issue, User } from '../../../server/src/types';
+import { canEditIssue, canDeleteIssue } from '../../../server/src/logic/permissions';
+
+describe('canEditIssue', () => {
+
+});
+
+describe('canDeleteIssue', () => {
+
+});
