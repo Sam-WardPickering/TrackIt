273fd7c48069487e9d3d32d15644ba91fe6ecafd
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-07-20 16:30:52 +1200

    Add unit test for password hash function

--- tests/unit/logic/auth.test.ts
+++ tests/unit/logic/auth.test.ts
@@ -2,6 +2,11 @@ import { describe, it, expect } from 'vitest';
 import { hashPassword, verifyPassword, signToken, verifyToken } from '../../../server/src/logic/auth';
 import type { AuthPayload, UserRole } from '../../../server/src/types/index';
 
-// describe('hashPassword', () => {
+describe('hashPassword', () => {
+    const password: string = 'Password123';
 
-// });
+    it('should not return the plaintext password', async () => {
+        const hash = await hashPassword(password);
+        expect(hash).not.toBe(password);
+    });
+});
