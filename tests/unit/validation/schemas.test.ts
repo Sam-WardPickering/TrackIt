4f5c71b3e6ddd2ca0f312519a5e00af16d7779b9
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-07-21 12:48:15 +1200

    Add file for zod schema unit tests

---
+++ tests/unit/validation/schemas.test.ts
@@ -0,0 +1,6 @@
+import { describe, it, expect } from 'vitest';
+import { registerSchema, loginSchema, createIssueSchema, updateIssueSchema } from '../../../server/src/validation/schemas';
+
+describe('registerSchema', () => {
+
+});
