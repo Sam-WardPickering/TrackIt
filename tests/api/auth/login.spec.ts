a5b1fae072683a8b10cd8a3b3757dbb51786fd91
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-08-03 20:48:46 +1200

    Add api test for invalid password when logging in

--- tests/api/auth/login.spec.ts
+++ tests/api/auth/login.spec.ts
@@ -29,3 +29,18 @@ test('POST /api/auth/login - invalid email', async ({ request }) => {
     const body = await response.json();
     expect(body.error).toBe('Invalid credentials');
 });
+
+
+test('POST /api/auth/login - invalid password', async ({ request }) => {
+    const response = await request.post('/api/auth/login', {
+        data: {
+            email: 'invalid.admin@trackit.test',
+            password: 'Password123', 
+        },
+    });
+
+    expect(response.status()).toBe(401);
+    
+    const body = await response.json();
+    expect(body.error).toBe('Invalid credentials');
+});
