6b75969cd8e98e4e18b16c0427da36a542ae9135
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-08-04 17:51:14 +1200

    Complete first valid credential register test case

--- tests/api/auth/register.api.spec.ts
+++ tests/api/auth/register.api.spec.ts
@@ -1,14 +1,19 @@
 import { test, expect } from '@playwright/test';
 
 test('POST /api/auth/register - valid credentials', async ({ request }) => {
-    const response = await request.post('/api/auth/register', {
-        data: {
-            email: 'newuser1@email.com',
-            password: 'newuserpassword123',
-            name: 'New User 1',
-        },
-    });
+    const userData = {
+        email: `testuser-${Date.now()}@test.com`,
+        password: 'newuserpassword123',
+        name: 'New User 1',
+    };
 
-    console.log(response.status());
-    console.log(await response.json());
+    const response = await request.post('/api/auth/register', { data: userData });
+
+    expect(response.status()).toBe(201);
+
+    const body = await response.json();
+    expect(body.token).toBeDefined();
+    expect(body.user.email).toBe(userData.email);
+    expect(body.user.name).toBe(userData.name);
+    expect(body.user.role).toBe('member');
 });
