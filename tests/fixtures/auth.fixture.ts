fc8ee6f5f0ccbc095100403bc0961a8dcc4d3276
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-08-05 16:07:17 +1200

    Formatting

--- tests/fixtures/auth.fixture.ts
+++ tests/fixtures/auth.fixture.ts
@@ -7,10 +7,10 @@ interface AuthFixtures {
 export const test = base.extend<AuthFixtures>({
     token: async ({ request }, use) => {
         const response = await request.post('/api/auth/login', {
-        data: {
-            email: 'admin@trackit.test',
-            password: 'Password123',
-        },
+            data: {
+                email: 'admin@trackit.test',
+                password: 'Password123',
+            },
     });
 
     const body = await response.json();
